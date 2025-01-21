f09db9a1def4326ae654d45392e424cf0bb32f83
Tutor Greg <tutorgreg@proton.me>
2025-01-21 09:51:01 -0500

    added mouse click and key press event handlers

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@
 
 int main()
 {
+    // Initialize window
     auto window = sf::RenderWindow(sf::VideoMode({800u, 800u}), "sfml-tests");
-    window.setFramerateLimit(144);
+    window.setFramerateLimit(60);
     
     const auto onClose = [&window](const sf::Event::Closed&)
     {
@@ -16,12 +17,27 @@ int main()
     {
         if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
             window.close();
+        else
+            std::cout << "Key pressed! Code: " << static_cast<int>(keyPressed.code) << ", Scancode: " << static_cast<int>(keyPressed.scancode) << std::endl;
+    };
+    
+    const auto onMouseClicked = [&window](const sf::Event::MouseButtonPressed& mouseClicked)
+    {
+        if (mouseClicked.button == sf::Mouse::Button::Left)
+        {
+            std::cout << "Left mouse button pressed at (" << mouseClicked.position.x << ", " << mouseClicked.position.y << ")" << std::endl;
+        }
+
+        else if (mouseClicked.button == sf::Mouse::Button::Right)
+        {
+            std::cout << "Right mouse button pressed at (" << mouseClicked.position.x << ", " << mouseClicked.position.y << ")" << std::endl;
+        }
     };
 
     while (window.isOpen())
     {
-        window.handleEvents(onClose, onKeyPressed);
-
+        window.handleEvents(onClose, onKeyPressed, onMouseClicked);
+        
         window.clear(sf::Color(50, 50, 50));
 
         window.display();
